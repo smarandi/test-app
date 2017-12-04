e01f08f0f0b1600d54a133d1f7138f32e77d9ebd
Samuel Marandi <samuel.marandi@evivehealth.com>
2017-12-04 23:35:45 +0530

    Static Campaign in more message

--- app/component/AppNavigation.js
+++ app/component/AppNavigation.js
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-multi-comp */
 // import React, { Component } from 'react';
 // import { Text, View, Button, StyleSheet, Image } from 'react-native';
+// import Quiz from './Quiz/Quiz';
 import { TabNavigator } from 'react-navigation';
 import Explore from './Explore/ExploreWrapper';
-// import Quiz from './Quiz/Quiz';
 import ScoreCard from './ScoreCard/ScoreCardWrapper';
 import More from './More/MoreWrapper';
 import COLORS from '../styles/Common/Colors';
--- app/component/More/More.js
+++ app/component/More/More.js
@@ -7,20 +7,20 @@ import MoreStyles from '../../styles/More/More';
 
 import CampaignImage from '../../assets/img/more/campaign.png';
 
-const ScreenWidth = Dimensions.get('window').width;
+// const ScreenWidth = Dimensions.get('window').width;
 // noinspection JSAnnotator
-const campaigns = [// [1, 2, 3, 4, 5];
-  {
-    image: CampaignImage,
-    title: 'Inspire\'s Corner: Campaign No. 1',
-  }, {
-    image: CampaignImage,
-    title: 'Inspire\'s Corner: Campaign No. 2',
-  }, {
-    image: CampaignImage,
-    title: 'Inspire\'s Corner: Campaign No. 3',
-  },
-];
+// const campaigns = [// [1, 2, 3, 4, 5];
+//   {
+//     image: CampaignImage,
+//     title: 'Inspire\'s Corner: Campaign No. 1',
+//   }, {
+//     image: CampaignImage,
+//     title: 'Inspire\'s Corner: Campaign No. 2',
+//   }, {
+//     image: CampaignImage,
+//     title: 'Inspire\'s Corner: Campaign No. 3',
+//   },
+// ];
 
 class More extends Component {
   static navigationOptions = {
@@ -29,33 +29,35 @@ class More extends Component {
     headerTitleStyle: { color: COLORS.WHITE },
   };
 
-  renderItem = campaign => // (<View key={campaign}><Text>{campaign}</Text></View>);
-    (
-      <View style={MoreStyles.campaignCarouselSection}>
-        <View style={MoreStyles.campaignCarouselContainer}>
-          <Image
-            source={CampaignImage}
-            style={MoreStyles.campaignImage}
-            resizeMode="cover"
-          />
-        </View>
-        <View style={MoreStyles.campaignTitleContainer}>
-          <Text style={MoreStyles.campaignTitle}>Campaign</Text>
-        </View>
-      </View>
-    );
-
   render() {
-    console.log('Here');
     return (
-      <View>
-        <Carousel
-          ref={(d) => { this._carousel = d; }}
-          data={campaigns}
-          renderItem={(item, index) => this.renderItem(item, index)}
-          sliderWidth="300"
-          itemWidth="300"
-        />
+      <View style={{ flex: 1 }}>
+        <View style={MoreStyles.campaignContainer}>
+          <Text style={MoreStyles.campaignContainerTitle}>Inspire's Corner</Text>
+          <View style={MoreStyles.campaignCarouselSection}>
+            <View style={MoreStyles.campaignCarouselContainer}>
+              <Image
+                source={CampaignImage}
+                style={MoreStyles.campaignImage}
+                resizeMode="cover"
+              />
+            </View>
+            <View style={MoreStyles.campaignTitleContainer}>
+              <Text style={MoreStyles.campaignTitle}>Inspire's Corner: Campaign No. 1</Text>
+            </View>
+          </View>
+        </View>
+        <View style={MoreStyles.socialMediaContainer}>
+          <Text>Social Media Links</Text>
+        </View>
+        <View style={MoreStyles.settingsContainer}>
+          <Text>Parent App</Text>
+          <Text>Your Profile</Text>
+        </View>
+        <View style={MoreStyles.aboutContainer}>
+          <Text>Help & Feedback</Text>
+          <Text>Terms & Conditions</Text>
+        </View>
       </View>
     );
   }
@@ -63,22 +65,12 @@ class More extends Component {
 
 export default More;
 
-//
-// <View style={{ flex: 1 }}>
-//   <View style={MoreStyles.campaignContainer}>
-//     <Text style={MoreStyles.campaignContainerTitle}>Inspire's Corner</Text>
-//
-//   </View>
-//   <View style={MoreStyles.socialMediaContainer}>
-//     <Text>Social Media Links</Text>
-//   </View>
-//   <View style={MoreStyles.settingsContainer}>
-//     <Text>Parent App</Text>
-//     <Text>Your Profile</Text>
-//   </View>
-//   <View style={MoreStyles.aboutContainer}>
-//     <Text>Help & Feedback</Text>
-//     <Text>Terms & Conditions</Text>
-//   </View>
-//
-// </View>
+
+/* <Carousel */
+/* ref={(d) => { this._carousel = d; }} */
+/* data= */
+/* renderItem={(item, index) => this.renderItem(item, index)} */
+/* sliderWidth={300} */
+/* itemWidth={300} */
+/* /> */
+
